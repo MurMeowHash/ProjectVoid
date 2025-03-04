67a17938bb8798af1afd1d54af8c440ca1a979cc
Oleksandr Havrysh <gavrishalex95@gmail.com>
2025-03-04 20:00:33 +0200

    Implementer core for application

---
+++ src/Core/Core.cpp
@@ -0,0 +1,62 @@
+#include "Core.h"
+#include "../Debug/Debug.h"
+#include "../Error/Error.h"
+
+namespace Core {
+    GLFWwindow *mainWindow;
+    int screenWidth;
+    int screenHeight;
+    const char *windowTitle;
+
+    void Initialize(int width, int height, const char *title, bool fullscreen) {
+        windowTitle = title;
+
+        glfwInit();
+        glfwSetErrorCallback([](int code, const char *description)
+        {
+            Debug::LogError(description);
+        });
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        CreateWindow(width, height, fullscreen);
+
+        glfwMakeContextCurrent(mainWindow);
+
+        if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            Error::FallWithFatalError("GLAD", "Failed to load GL functions");
+        }
+    }
+
+    void CreateWindow(int width, int height, bool fullscreen) {
+        GLFWmonitor *targetMonitor;
+        if(fullscreen) {
+            targetMonitor = glfwGetPrimaryMonitor();
+            const GLFWvidmode *monitorProperties = glfwGetVideoMode(targetMonitor);
+            screenWidth = monitorProperties->width;
+            screenHeight = monitorProperties->height;
+        } else {
+            targetMonitor = nullptr;
+            screenWidth = width;
+            screenHeight = height;
+        }
+
+        mainWindow = glfwCreateWindow(screenWidth, screenHeight, windowTitle, targetMonitor, nullptr);
+        if(!mainWindow) {
+            Error::FallWithFatalError("GLFW", "Failed to create window");
+        }
+    }
+
+    bool WindowShouldClose() {
+        return glfwWindowShouldClose(mainWindow);
+    }
+
+    void FinishFrame() {
+        glfwPollEvents();
+        glfwSwapBuffers(mainWindow);
+    }
+
+    void Terminate() {
+        glfwTerminate();
+    }
+}
