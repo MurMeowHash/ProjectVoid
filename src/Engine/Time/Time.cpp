087aa5c5d7c977dba58b4baf3243097a522ab1af
Oleksandr Havrysh <gavrishalex95@gmail.com>
2025-03-05 11:39:25 +0200

    Introduced delta time

---
+++ src/Engine/Time/Time.cpp
@@ -0,0 +1,18 @@
+#include "Time.h"
+#include <GLFW/glfw3.h>
+
+
+namespace Time {
+    float prevTime = 0.0f;
+    float deltaTime;
+
+    void UpdateDeltaTime() {
+        auto currentTime = static_cast<float>(glfwGetTime());
+        deltaTime = currentTime - prevTime;
+        prevTime = currentTime;
+    }
+
+    float GetDeltaTime() {
+        return deltaTime;
+    }
+}
