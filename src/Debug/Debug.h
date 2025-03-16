2c70cfd803c7c48893e3098a047505a01df8ddad
Oleksandr Havrysh <gavrishalex95@gmail.com>
2025-03-16 12:26:58 +0200

    Warning added

--- src/Debug/Debug.h
+++ src/Debug/Debug.h
@@ -10,15 +10,25 @@ namespace Debug {
     void Log(Args ... args) {
         if(!Engine::IsDebugMode()) return;
 
-        ((*debugStream << args << " "), ...);
+        ((*debugStream << args), ...);
+    }
+
+    template<typename ... Args>
+    void LogCritical(const std::string &criticalType, Args ... args) {
+        if(!Engine::IsDebugMode()) return;
+
+        *debugStream << criticalType;
+        ((*debugStream << " > " << args), ...);
+        *debugStream << '\n';
     }
 
     template<typename ... Args>
     void LogError(Args ... args) {
-        if(!Engine::IsDebugMode()) return;
+        LogCritical("Error", args...);
+    }
 
-        *debugStream << "Error";
-        ((*debugStream << " > " << args), ...);
-        *debugStream << '\n';
+    template<typename ... Args>
+    void LogWarning(Args ... args) {
+        LogCritical("Warning", args...);
     }
 }
