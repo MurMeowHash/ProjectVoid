f9b3ca2033946af57d2ef8eb8ac1bc1f4dd4c96e
Oleksandr Havrysh <gavrishalex95@gmail.com>
2025-04-11 20:40:25 +0300

    Fixed structure

--- src/Core/Resources/ResourceManager.h
+++ src/Core/Resources/ResourceManager.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "../../Game/Types/Texture/Texture2D.h"
+#include "../../Game/Types/GLBuffer/Texture/Texture2D.h"
 #include <string>
 #include "../../Utils/Utils.h"
 #include "../../Game/Types/Model/Model.h"
@@ -8,7 +8,6 @@
 
 namespace ResourceManager {
     constexpr ModelLoadParameters DEFAULT_MODEL_LOAD_PARAMS = ModelLoadParameters();
-    constexpr int ABSENT_RESOURCE = -1;
 
     void LoadModel(const char *path, const ModelLoadParameters &params = DEFAULT_MODEL_LOAD_PARAMS);
     int LoadTexture(const char *path, TextureParameters &params);
@@ -16,6 +15,8 @@ namespace ResourceManager {
                        const TextureParameters &params = TextureParameters(), unsigned char *data = nullptr);
     void CreateMaterial(const std::string &name, int albedoMap = ABSENT_RESOURCE, int normalMap =
             ABSENT_RESOURCE, int specularMap = ABSENT_RESOURCE, float shininess = Material::DEFAULT_SHININESS);
+    void CreateManualMesh(const std::string &name, const std::vector<Vertex> &vertices, const std::vector<uint> &indices,
+                          bool hasNormals = false, bool hasTangents = false);
     Texture2D* GetTextureByIndex(int index);
     Model* GetModelByIndex(int index);
     Mesh* GetMeshByIndex(int index);
